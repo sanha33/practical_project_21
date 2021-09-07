6a33c437748e1840a433e11bce0f90db26b0bbbe
mount <mountrive@naver.com>
2021-09-07 16:31:53 +0900

    third push

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		System.out.println("Hello World!!!\n");
+		System.out.println("second push!!!!!\n");
 		System.out.print("이름 입력 :");
 		String name = s.next();
 		System.out.println("["+name+"]님 환영합니다~");
